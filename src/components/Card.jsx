c817619d979a3582acfbfb79bc27db2b03892b6d
JustoDMeiners <justito_dm@hotmail.com>
2022-11-08 13:19:19 -0300

    commiteando

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = ( {alumno} ) => {
+
+  const [borrar, setBorrar] = useState(alumno.legajo.nombre)
+
+  const borrarNombre = () =>{
+    setBorrar(alumno.legajo.nombre = "")
+
+    
+  }
   
   return (
     <div className='producto'>
-      <h2> Nombre: {alumno.legajo.nombre} </h2>
+      <h2 className='h2'> Nombre: {alumno.legajo.nombre} </h2>
       <h3>Edad: {alumno.legajo.edad}</h3>
       <h3>Materias probadas: {alumno.materiasAprobadas}</h3>
-      <button type='button'>Borrar Alumno</button>
+      <button type='button' onClick={borrarNombre}>Borrar nombre</button>
     </div>
   )
 }
